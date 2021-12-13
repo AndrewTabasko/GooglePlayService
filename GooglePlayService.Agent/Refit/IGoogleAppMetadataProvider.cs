8365927df6f69ed9c5d5d029c7e83fc3c91fb260
andreytabasko2.0@gmail.com <andreytabasko2.0@gmail.com>
2021-12-13 14:03:37 +0300

    debug complete succsessfully, not access to backend container

--- GooglePlayService.Agent/Refit/IGoogleAppMetadataProvider.cs
+++ GooglePlayService.Agent/Refit/IGoogleAppMetadataProvider.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Refit;
@@ -9,6 +10,6 @@ namespace GoogleApps.Agent.Refit
     public interface IGoogleAppMetadataProvider
     {
         [Get("/api/apps/{query}")]
-        Task<string> AppMetadataRequestApi(string query);// attribute query or path
+        Task<string> AppMetadataRequestApi([Query]string query);// attribute query or pathewq
     }
 }
